f45338e42ba94b96c1ba9e7d3c25cfc1c706a900
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-01-11 13:05:27 +0600

    Customer Registration From Complete

---
+++ shop/forms.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.forms import ModelForm, fields
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterCustomer(UserCreationForm):
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.CharField(label='email', widget=forms.EmailInput(attrs={'placeholder': 'example@mail.com'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'placeholder': 'username123'}))
+
+    contact = forms.CharField(label='contact', widget=forms.NumberInput(attrs={'placeholder': '017XXXXXXXX'}))
+    social_media_link = forms.CharField(label='Social Link', widget=forms.URLInput(attrs={'placeholder': 'https://www.facebook.com/example'}))
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2','first_name', 'last_name', 'email', 'contact', 'social_media_link']
--- shop/views/public.py
+++ shop/views/public.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.contrib.auth.models import Group
+from django.forms.models import inlineformset_factory
+from django.shortcuts import redirect, render
+from django.http import HttpRequest,HttpResponseRedirect
+from django.urls import reverse
+
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.urls.conf import path
+from shop.forms import RegisterCustomer
+
+from shop.models import Customer
 
 def welcome(request):
     contex = {
         'title' : 'Home Page',
+        'h1_tag' : 'The New Day (TND) is a Cloud Kitchen of Fast Food & Restaurant with Multi Cuisine',
         'class' : 'index-template fastfood_1'
     }
     return render(request,'welcome.html', contex)
@@ -11,6 +22,7 @@ def welcome(request):
 def category(request):
     context = {
         'title' : 'Category Collections',
+        'h1_tag' : 'The New Day (TND) is a Cloud Kitchen of Fast Food & Restaurant with Multi Cuisine',
         'class' : 'fastfood_1'
     }
 
@@ -19,6 +31,7 @@ def category(request):
 def categoryProducts(request):
     context = {
         'title' : 'Products by Category',
+        'h1_tag' : 'The New Day (TND) is a Cloud Kitchen of Fast Food & Restaurant with Multi Cuisine',
         'class' : 'fastfood_1'
     }
 
@@ -27,15 +40,37 @@ def categoryProducts(request):
 def details(request):
     context = {
         'title' : 'Product Detail',
+        'h1_tag' : 'The New Day (TND) is a Cloud Kitchen of Fast Food & Restaurant with Multi Cuisine',
         'class' : 'fastfood_1'       
     }
 
     return render(request,'product_details.html',context)
 
 def registration(request):
+    form = RegisterCustomer
     context = {
         'title' : 'Registration | Customer',
-        'class' : 'fastfood_1'  
+        'h1_tag' : 'The New Day (TND) is a Cloud Kitchen of Fast Food & Restaurant with Multi Cuisine',
+        'form_title' : 'Customer Registration',
+        'class' : 'fastfood_1',  
+        'form' : form
     }
 
-    return render(request, 'customer_registration.html', context)
+    if request.method == 'POST':
+        form = RegisterCustomer(request.POST)
+        if form.is_valid():
+            user = form.save()  #insert
+            #Get contact number from form and store at phone 
+            phone = form.cleaned_data.get('contact')
+            #Get Social Link from form and store at link 
+            link = form.cleaned_data.get('social_media_link')
+
+            group = Group.objects.get(name='Customer')
+            #insert Group type at auth_user_groups table
+            user.groups.add(group)
+            Customer.objects.create(contact = phone, social_media_link = link, user_id = user)
+            return redirect('welcome')
+    elif request.method == 'GET':
+        return render(request, 'customer_registration.html', context)
+    else:
+        return HttpResponseRedirect(reverse('welcome'))
